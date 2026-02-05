78631c71a4a1eedebf155bbc67b874bb64f2c7be
Irfan Hakim <irfanhakim.as@yahoo.com>
2026-02-05 21:49:56 +0800

    Add data module to consolidate all json data

---
+++ src/data/data.js
@@ -0,0 +1,11 @@
+const site = require("./site.json");
+const cat = require("./cat.json");
+const health = require("./health.json");
+const owner = require("./owner.json");
+
+module.exports = {
+    site,
+    cat,
+    health,
+    owner,
+};
