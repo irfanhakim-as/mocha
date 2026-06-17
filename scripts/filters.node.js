bd066c5d1d7343d671244ff12df681b4dcb04cff
Irfan Hakim <irfanhakim.as@yahoo.com>
2026-06-18 00:23:39 +0800

    Set markdown links to open in new tab
    Like other user-supplied links on the site

--- scripts/filters.node.js
+++ scripts/filters.node.js
@@ -1,6 +1,12 @@
 const crypto = require('crypto');
 const markdownIt = require('markdown-it');
 const _md = markdownIt({ html: false, linkify: true });
+// Open links in new tab
+_md.renderer.rules.link_open = function(tokens, idx, options, env, self) {
+    tokens[idx].attrSet('target', '_blank');
+    tokens[idx].attrSet('rel', 'noopener noreferrer');
+    return self.renderToken(tokens, idx, options);
+};
 
 // Render markdown string as inline HTML
 function markdownInline(value) {
